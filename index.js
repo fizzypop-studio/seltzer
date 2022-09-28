2c7cb3794e2917008bda3b26e5a7f512d1668f5e
sethcarrino <sethcarrino@gmail.com>
2022-09-28 09:13:25 -0400

    version bump

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ cli.name("seltzer")
     .option("--client", "Frontend React Client Only")
     .option("--api", "Rails API Only")
     .addHelpCommand(false)
-    .version('1.0.2');
+    .version('1.0.3');
 
 cli.command('create')
     .description('scaffold separate projects for a React client and Rails API')
