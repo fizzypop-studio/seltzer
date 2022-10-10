db1a4ede2b06e0e1272d5eb5ee74326c3afcf4c2
sethcarrino <sethcarrino@gmail.com>
2022-10-10 13:59:57 -0400

    allow for client only option to pull from client url

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const seltzerChalkColor = chalk.hex('#fc0362');
 const seltzerASCII = seltzerChalkColor(` ________  _______   ___   _________  ________  _______   ________\r\n|\\   ____\\|\\  ___ \\ |\\  \\ |\\___   ___\\\\_____  \\|\\  ___ \\ |\\   __  \\\r\n\\ \\  \\___|\\ \\   __\/|\\ \\  \\\\|___ \\  \\_|\\|___\/  \/\\ \\   __\/|\\ \\  \\|\\  \\\r\n \\ \\_____  \\ \\  \\_|\/_\\ \\  \\    \\ \\  \\     \/  \/ \/\\ \\  \\_|\/_\\ \\   _  _\\\r\n  \\|____|\\  \\ \\  \\_|\\ \\ \\  \\____\\ \\  \\   \/  \/_\/__\\ \\  \\_|\\ \\ \\  \\\\  \\|\r\n    ____\\_\\  \\ \\_______\\ \\_______\\ \\__\\ |\\________\\ \\_______\\ \\__\\\\ _\\\r\n   |\\_________\\|_______|\\|_______|\\|__|  \\|_______|\\|_______|\\|__|\\|__|\r\n   \\|_________|`);
 const appURL = 'https://github.com/fizzypop-studio/seltzer-app.git#main';
 const apiURL = 'https://github.com/fizzypop-studio/seltzer-api.git#main';
+const clientURL = 'https://github.com/fizzypop-studio/seltzer-client.git#main';
 
 cli.name("seltzer")
     .description(chalk.cyan("Build React and Rails projects in no time"))
@@ -89,12 +90,39 @@ cli.command('create')
             })
         }
 
-        if(options.client || isOptionsEmpty) {
-            downloadApp();
+        const downloadClientOnly = () => {
+            console.log(chalk.green('\n Start generating React client...'))
+            
+            //The load icon appears
+            const spinner = ora(`Downloading from ${clientURL}`)
+            spinner.start()
+
+            download(`direct:${clientURL}`, `./${projectName}-app`, { clone: true }, (err) => {
+                if (err) {
+                    spinner.fail()
+                    console.log(chalk.red(symbols.error), chalk.red(`React Project Generation failed. ${err}`))
+                    return
+                }
+                // End loading Icon
+                spinner.succeed()
+                console.log(chalk.green(symbols.success), chalk.green('React Project Generation completed!'))
+                console.log('\n To get started with your React Client Project')
+                console.log(chalk.cyan(`\n    1. cd ${projectName}-app`))
+                console.log(chalk.cyan(`\n    2. yarn install`))
+                console.log(chalk.cyan(`\n    3. yarn start \n`))
+            })
+        }
+
+        if(isOptionsEmpty) {
+            downloadApp()
         }
 
         if(options.api) {
             downloadAPI();
+        }
+
+        if(options.client) {
+            downloadClientOnly();
         }
     });
 
